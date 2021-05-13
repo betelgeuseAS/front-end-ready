360d8b9dc97eb80399d91ce8b4d62412d51b972a
DESKTOP-KIKRGFV\betel <betelgeuse.as@gmail.com>
2021-05-13 22:31:46 +0300

    forming html

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,7 +1,12 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 
 export class Formula extends ExcelComponent {
+  static className = 'excel_formula'
+
   toHTML() {
-    return super.toHTML()
+    return `
+      <div class="info">fx</div>
+      <div class="input" contenteditable spellcheck="false"></div>
+    `
   }
 }
