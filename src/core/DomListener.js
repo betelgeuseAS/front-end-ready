640e26c3902da1d3a5f0520d8b811c7b45a9d184
DESKTOP-KIKRGFV\betel <betelgeuse.as@gmail.com>
2021-05-16 22:23:49 +0300

    add event listeners

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,5 @@
+import {capitalize} from '@core/utils';
+
 export class DomListener {
   constructor($root, listeners = []) {
     if (!$root) {
@@ -9,10 +11,24 @@ export class DomListener {
   }
 
   initDomListeners() {
-    console.log(this.listeners)
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      if (!this[method]) {
+        const name = this.name || ''
+
+        throw new Error(
+            `Method ${method} is not implemented in ${name} Component`
+        )
+      }
+      this.$root.on(listener, this[method].bind(this))
+    })
   }
 
   removeDomListeners() {
 
   }
+}
+
+function getMethodName(eventName) {
+  return 'on' + capitalize(eventName)
 }
